e79f71d7762b30663176bf62b5fcac7c84871767
Ducreus <ducanhlaminh@gmail.com>
2022-08-23 22:42:51 +0700

    base-v-1.0

--- src/containers/public/Header.js
+++ src/containers/public/Header.js
@@ -1,47 +1,50 @@
-import React from 'react'
-import { Button } from '../../components'
-import { useSelector, useDispatch } from 'react-redux'
-import * as actions from '../../store/actions'
-import { useNavigate } from 'react-router-dom'
-
+import React from "react";
+import { Button } from "../../components";
+import { useSelector, useDispatch } from "react-redux";
+import * as actions from "../../store/actions";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isLoggedIn, userCurrent } = useSelector((state) => state.auth);
+  return (
+    <div className="w-full flex items-center gap-5 py-5 justify-center">
+      {isLoggedIn && <small>{`xin chào ${userCurrent?.name}`}</small>}
+      {!isLoggedIn && (
+        <>
+          <Button
+            text={"Đăng nhập"}
+            bgColor="bg-blue-700"
+            textColor="text-white"
+            onClick={() => navigate("/login", { state: { flag: false } })}
+          />
+          <Button
+            text={"Đăng ký"}
+            bgColor="bg-blue-700"
+            textColor="text-white"
+            onClick={() => navigate("/login", { state: { flag: true } })}
+          />
+        </>
+      )}
+      {isLoggedIn && (
+        <>
+          <Button
+            text={"Đăng xuất"}
+            bgColor="bg-red-700"
+            textColor="text-white"
+            onClick={() => dispatch(actions.logout())}
+          />
+          <Button
+            text={"Quản lý"}
+            bgColor="bg-sky-700"
+            textColor="text-white"
+            onClick={() => navigate("/system/")}
+          />
+        </>
+      )}
+    </div>
+  );
+};
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const { isLoggedIn, userCurrent } = useSelector(state => state.auth)
-    return (
-        <div className='w-full flex items-center gap-5 py-5 justify-center' >
-            {isLoggedIn && <small>{`xin chào ${userCurrent?.name}`}</small>}
-            {!isLoggedIn && <>
-                <Button
-                    text={'Đăng nhập'}
-                    bgColor='bg-blue-700'
-                    textColor='text-white'
-                    onClick={() => navigate('/login', { state: { flag: false } })}
-                />
-                <Button
-                    text={'Đăng ký'}
-                    bgColor='bg-blue-700'
-                    textColor='text-white'
-                    onClick={() => navigate('/login', { state: { flag: true } })}
-                /></>}
-            {isLoggedIn && <>
-                <Button
-                    text={'Đăng xuất'}
-                    bgColor='bg-red-700'
-                    textColor='text-white'
-                    onClick={() => dispatch(actions.logout())}
-                />
-                <Button
-                    text={'Quản lý'}
-                    bgColor='bg-sky-700'
-                    textColor='text-white'
-                    onClick={() => navigate('/system/')}
-                />
-            </>}
-        </div>
-    )
-}
-
-export default Header
+export default Header;
--- src/containers/public/Home.js
+++ src/containers/public/Home.js
@@ -1,16 +1,14 @@
-import React, { useEffect } from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './Header'
-
+import React, { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
 
 const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <Header />
-            <Outlet />
-        </div>
-    )
-}
-
-export default Home
+export default Home;
