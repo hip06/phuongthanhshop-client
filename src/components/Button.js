b5532ca7d4b5a03398bc5dafa471c200285f7b8b
tuấn anh đinh <tuananhd.020302@gmail.com>
2022-08-26 16:26:54 +0700

    Create Input-field 26/8

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,23 +11,35 @@ const Button = ({ text, bgColor, textColor, onClick, fullWidth }) => {
         </button>
     )
 }
-export const ButtonSmall=({content,color})=>{
-    
-    return (  
-        <button className="text-base text-white px-[15px] py-[9px] rounded-lg hover:-translate-y-0.5 transition-all ease-in-out active:translate-y-0.5 mr-[20px]" style={{backgroundColor:color}}>{content}</button>
+export const ButtonSmall = ({ content, color }) => {
+
+    return (
+        <button className="text-base text-white px-[15px] py-[9px] rounded-lg hover:-translate-y-0.5 transition-all ease-in-out active:translate-y-0.5 mr-[20px]" style={{ backgroundColor: color }}>{content}</button>
     );
 }
 
-export const ButtonMedium=({content,color})=>{
-    
-    return (  
-        <button className="text-base text-white px-10 py-5 rounded-lg hover:-translate-y-0.5 transition-all ease-in-out active:translate-y-0.5 mr-[20px]" style={{backgroundColor:color}}>{content}</button>
+export const ButtonMedium = ({ content, color }) => {
+
+    return (
+        <button className="text-base text-white px-10 py-5 rounded-lg hover:-translate-y-0.5 transition-all ease-in-out active:translate-y-0.5 mr-[20px]" style={{ backgroundColor: color }}>{content}</button>
     );
 }
-export const ButtonLarge=({content,color})=>{
-    
-    return (  
-        <button className="text-base text-white px-16 py-5 rounded-lg hover:-translate-y-0.5 transition-all ease-in-out active:translate-y-0.5 mr-[20px]" style={{backgroundColor:color}}>{content}</button>
+export const ButtonLarge = ({ content, color }) => {
+
+    return (
+        <button className="text-base text-white px-16 py-5 rounded-lg hover:-translate-y-0.5 transition-all ease-in-out active:translate-y-0.5 mr-[20px]" style={{ backgroundColor: color }}>{content}</button>
+    );
+}
+
+export const ButtonCtWidth = ({ content, color, width, height, onAction }) => {
+
+    return (
+        <button className={`text-base text-white 
+        px-${width} py-${height} rounded-lg 
+        hover:-translate-y-0.5 transition-all 
+        ease-in-out active:translate-y-0.5 
+        mr-[20px] bg-[${color}]`}
+            onClick={() => onAction()}>{content}</button>
     );
 }
 
--- src/containers/system/CreateProduct.js
+++ src/containers/system/CreateProduct.js
@@ -1,15 +1,98 @@
-import React, { useState } from 'react'
-import InputCustomWidth from '../../components/InputCtWidth'
+import React, { useEffect, useState } from 'react'
+import {
+    InputCustomWidth,
+    SelectCustomWidth,
+    HashTagCustomWidth,
+    TextCustomWidth,
+    InputFileCustomWidth
+} from '../../components/InputCtWidth'
+import Loading from '../../components/Loading'
 
 const CreateProduct = () => {
-    const [test, setTest] = useState('this is test value')
+    const [productName, setProductName] = useState('')
+    const [options, setOptions] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [selectValue, setSelectValue] = useState('')
+    const [price, setPrice] = useState('')
+    const [tags, setTags] = useState([])
+    const [shortDes, setShortDes] = useState('')
+
+    const exampleArray = ['Option 1', 'option 2', 'Option 3', 'Option 4']
+    useEffect(() => {
+        setOptions([...exampleArray])
+        setLoading(false)
+        setSelectValue('option 1')
+    }, [])
 
     return (
         <>
-            <InputCustomWidth
-                value={test}
-                lable='test field'
-            />
+            {loading ? <Loading />
+                :
+                <>
+                    <h1 className="text-3xl">Nhập thông tin tại đây</h1>
+                    <div className="w-full items-center bg-[#d9d9d9] rounded p-3 justify-between p-5">
+                        <InputCustomWidth
+                            widthP={'full'}
+                            lable='Tên sản phẩm '
+                            placeholder='Tên sản phẩm...'
+                            PLarge={true}
+                            productName={productName}
+                            setProductName={setProductName}
+                        />
+                        <div className='flex w-[100%]'>
+                            <div className="flex">
+                                <SelectCustomWidth
+                                    widthP='[300px]'
+                                    lable='Loại hàng'
+                                    options={options}
+                                    selectValue={selectValue}
+                                    setSelectValue={setSelectValue}
+                                />
+                            </div>
+                            <div className="flex w-[220px] ml-[24px] mr-[24px]">
+                                <InputCustomWidth
+                                    widthP={'full'}
+                                    lable='Giá'
+                                    placeholder='Giá: VND'
+                                    PLarge={false}
+                                    price={price}
+                                    setPrice={setPrice}
+                                />
+                            </div>
+                            <div className="flex w-[580px] ml-[12px]">
+                                <HashTagCustomWidth
+                                    widthP={'full'}
+                                    lable='Hash_Tag'
+                                    placeholder='Tag...'
+                                    tags={tags}
+                                    setTags={setTags}
+                                />
+                            </div>
+                        </div>
+                        <div className="flex">
+                            <div className=" w-[46%] mr-[24px]">
+                                <InputCustomWidth
+                                    widthP={'full'}
+                                    lable='Miêu tả ngắn gọn'
+                                    placeholder='Miêu tả...'
+                                    PLarge={true}
+                                    shortDes={shortDes}
+                                    setShortDes={setShortDes}
+                                />
+                                <p className='text-sm'>Hiển thị trên thẻ sản phẩm</p>
+                                <TextCustomWidth
+                                    widthP='full'
+                                    lable='Miêu tả chi tiết'
+                                    placeholder='Miêu tả chi tiết tại đây...'
+                                />
+                            </div>
+                            <div className=" w-[50%]">
+                                <InputFileCustomWidth />
+                            </div>
+                        </div>
+                    </div>
+                </>}
+
         </>
     )
 }
