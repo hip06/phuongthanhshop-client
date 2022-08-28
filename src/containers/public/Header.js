02e160970e72a82dd32920a117d6886f4d7ad0cf
Ducreus <ducanhlaminh@gmail.com>
2022-08-28 22:00:02 +0700

    Update Header.js

--- src/containers/public/Header.js
+++ src/containers/public/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import logofashion from "../../assets/logofashion.png";
-import { HiOutlineMenu } from "react-icons/hi"
+import { HiOutlineMenu } from "react-icons/hi";
 import { BiSearchAlt, BiUser } from "react-icons/bi";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import LayoutMenu from "../Layout/LayoutMenu";
@@ -9,17 +9,34 @@ const Header = () => {
   const [modalShow, setModalShow] = useState(false);
   return (
     <div className="flex items-center justify-around relative">
-      <div className=" " onClick={() => { setModalShow(true) }}>
-        <HiOutlineMenu size={26} ></HiOutlineMenu>
+      <div
+        className=" "
+        onClick={() => {
+          setModalShow(true);
+        }}
+      >
+        <HiOutlineMenu size={26}></HiOutlineMenu>
       </div>
-      {modalShow && <div className="z-10 top-0 left-0 w-full fixed h-full" onClick={(e) => {setModalShow(false) }}>
-        <div className="w-[80%] absolute z-100" onClick={(e) => {e.stopPropagation();}}>
-          <LayoutMenu></LayoutMenu>
+      {modalShow && (
+        <div
+          className="z-10 top-0 left-0 w-full fixed h-full"
+          onClick={(e) => {
+            setModalShow(false);
+          }}
+        >
+          <div
+            className="w-[80%] absolute z-100"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <LayoutMenu></LayoutMenu>
+          </div>
         </div>
-      </div>}
+      )}
 
       <div>
-        <BiSearchAlt size={26} ></BiSearchAlt>
+        <BiSearchAlt size={26}></BiSearchAlt>
       </div>
 
       <div>
@@ -27,15 +44,14 @@ const Header = () => {
       </div>
 
       <div>
-        <BiUser size={26} ></BiUser>
+        <BiUser size={26}></BiUser>
       </div>
 
       <div>
-        <AiOutlineShoppingCart size={26} ></AiOutlineShoppingCart>
+        <AiOutlineShoppingCart size={26}></AiOutlineShoppingCart>
       </div>
     </div>
   );
-
 };
 
 export default Header;
